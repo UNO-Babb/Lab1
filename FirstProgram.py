e00591a7322fe54df2fdbcba720fa1dc0ff4d0b7
Derek Babb <dbabb1@gmail.com>
2019-12-12 12:04:39 -0600

    Initial shell for the first program

---
+++ FirstProgram.py
@@ -0,0 +1,16 @@
+#FirstProgram.py
+#Name:
+#Date:
+#Assignment:
+
+def main():
+  #Say hello
+
+  #Ask for the user's name
+
+  #Use the user's name in the program.
+
+  #Ask the user for their age.
+
+  
+main()
